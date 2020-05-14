9a43baa57f7692055c21cdb3dad5362fe62f3f24
fcvlad <fcvlad@gmail.com>
2020-05-14 16:57:32 +0800

    添加项目文件。

---
+++ FTP/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace FTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnUpload = new DevExpress.XtraEditors.SimpleButton();
+            this.progressBarControl = new DevExpress.XtraEditors.ProgressBarControl();
+            this.lblStatus = new DevExpress.XtraEditors.LabelControl();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.btnUpload.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.btnUpload.Location = new System.Drawing.Point(244, 51);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(105, 80);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // progressBarControl
+            // 
+            this.progressBarControl.Location = new System.Drawing.Point(103, 137);
+            this.progressBarControl.Name = "progressBarControl";
+            this.progressBarControl.Size = new System.Drawing.Size(417, 20);
+            this.progressBarControl.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(267, 174);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(60, 14);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Upload 0%";
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 299);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBarControl);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton btnUpload;
+        private DevExpress.XtraEditors.ProgressBarControl progressBarControl;
+        private DevExpress.XtraEditors.LabelControl lblStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
+
---
+++ FTP/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace FTP
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        struct ftpsetting
+        {
+            public string Filename { get; set; }
+            public string Fullname { get; set; }
+
+        }
+        ftpsetting _inputPara;
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string fileName = ((ftpsetting)e.Argument).Filename;
+            string fullName = ((ftpsetting)e.Argument).Fullname;
+            string ftpaddres = "192.168.0.199";
+            string ftpuser = "admin";
+            string ftppassword = "123456";
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format("{0}/{1}",ftpaddres,fileName)));
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(ftpuser,ftppassword);
+            Stream ftpStream = request.GetRequestStream();
+            FileStream fs = File.OpenRead(fullName);
+            byte[] buffer = new byte[1024];
+            double total = (double)fs.Length;
+            int byteRead = 0;
+            double read = 0;
+            do
+            {
+                if (!backgroundWorker.CancellationPending)
+                {
+                    byteRead = fs.Read(buffer, 0, 1024);
+                    ftpStream.Write(buffer, 0, byteRead);
+                    read += (double)byteRead;
+                    double percentage = read / total * 100;
+                    backgroundWorker.ReportProgress((int)percentage);
+                }
+            }
+            while (byteRead != 0);
+            fs.Close();
+            ftpStream.Close();
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.lblStatus.Text = $"Uploaded{e.ProgressPercentage}%";
+            this.progressBarControl.EditValue = e.ProgressPercentage;
+            this.progressBarControl.Update();
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.lblStatus.Text = "Upload Complete!";
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Multiselect = false, ValidateNames = true, Filter = "All file | *.*" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fi = new FileInfo(ofd.FileName);
+                    _inputPara.Filename = fi.Name;
+                    _inputPara.Filename = fi.FullName;
+                }
+            }
+        }
+    }
+}
